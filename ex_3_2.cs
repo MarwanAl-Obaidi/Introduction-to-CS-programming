0e7b835273c609207f5e5e9470a21389e586ebb6
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-01-11 13:33:37 +0200

    Exercises 3_1 to 3_3 upload

---
+++ ex_3_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            Console.Write("What is your favourite fruit? ");
+            answer = Console.ReadLine();
+            Console.WriteLine("Your favourite fruit is " + answer);
+        }
+    }
+}
---
+++ ex_3_3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The command line argument was: ");
+            Console.WriteLine(args[0]);
+        }
+    }
+}
